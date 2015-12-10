2598996130c4966059c2e57c6ccb32c3eebb3baf
Luke Rohde <lukerohde@gmail.com>
2015-12-10 17:26:48 +1100

    Tests around person to union assignment and security, forbade editing people outside your union, removed new & create person actions

--- features/step_definitions/shared.rb
+++ features/step_definitions/shared.rb
@@ -13,3 +13,12 @@ Given /^I am a new, authenticated person$/ do
   fill_in "person_password", :with => password
   click_button "Log in"
 end
+
+Given /^I am authenticated as admin$/ do
+  # Admin is created by seed
+	@current_person = Person.find_by_first_name('Admin')
+  visit '/people/sign_in'
+  fill_in "person_email", :with => @current_person.email
+  fill_in "person_password", :with => 'temptemp'
+  click_button "Log in"
+end
