cdff476d2b0e0aff95b5b511fe7bd6fed91c26bb
Luke Rohde <lukerohde@gmail.com>
2016-03-09 12:33:03 +1100

    add option to delete chat messages. And fix broken test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,6 +15,23 @@ class MessagesController < ApplicationController
 		@message = Message.create!(message_params)
 	end
 
+	# DELETE /comments/1.json
+  def destroy
+    @message = Message.find(params[:id])
+
+    respond_to do |format|
+    	if @message.person == current_person || owner? 
+      	if @message.destroy
+      		format.js { render :destroy }
+      	else 
+      		format.js { render js: "alert('Whoops!  Something went wrong...');" } # unprocessible 
+    		end
+    	else
+    		format.js { render js: "alert('You are forbidden');" } # forbidden 
+    	end
+    end
+  end
+
  private
  	def message_params
  		  params.require(:message).permit(:body, :person_id, :display_name)
--- features/step_definitions/union_company_steps.rb
+++ features/step_definitions/union_company_steps.rb
@@ -48,7 +48,8 @@ Then(/^I can delete the "(.*?)" titled "(.*?)"$/) do |entity_name, title|
 	entity.should_not eq(nil)
 
 	click_link title
-	click_link '', edit_polymorphic_path(entity)
+	#click_link '', edit_polymorphic_path(entity)
+	find(:xpath, "//a[@href='#{edit_polymorphic_path(entity)}']").click
 	click_link "Permanently Delete This #{klass.to_s}" # TODO Assumes only one company in list
 
 	#click_link "Destroy" # TODO Assumes only one company in list
