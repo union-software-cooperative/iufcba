1a6cdf2ae5e7ce594f0fab4afde0bf4f3208c1a6
Luke Rohde <lukerohde@gmail.com>
2016-08-09 17:04:21 +1000

    Nature of operations and logo

--- app/helpers/recs_helper.rb
+++ app/helpers/recs_helper.rb
@@ -2,7 +2,7 @@ module RecsHelper
 	
 	def nature_of_operation_options
 		options_for_select(
-				(%w[fresh-milk powdered-milk butter cheese yogurt ice-cream cream industrial-dairy food-service soy-milk other-specific] + @rec.nature_of_operation).uniq, 
+				(%w[beer PepsiCo Coca-Cola other-beverages] + @rec.nature_of_operation).uniq, 
 				@rec.nature_of_operation
 			)
 	end
