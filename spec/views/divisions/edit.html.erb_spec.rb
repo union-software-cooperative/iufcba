b7ecbfbf78a438517386cda13bd38b0ebc4fe5b2
Kim Buckley <kbuckley@nuw.org.au>
2017-02-09 14:36:03 +1100

    Rails l10n file for basic Spanish, improved i18n coverage, extra tests

--- spec/views/divisions/edit.html.erb_spec.rb
+++ spec/views/divisions/edit.html.erb_spec.rb
@@ -14,7 +14,6 @@ describe "divisions/edit" do
   it "renders the edit division form" do
     render
 
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", division_path(@division), "post" do
       assert_select "input#division_name[name=?]", "division[name]"
       assert_select "input#division_short_name[name=?]", "division[short_name]"
@@ -22,5 +21,12 @@ describe "divisions/edit" do
       assert_select "input#division_colour1[name=?]", "division[colour1]"
       assert_select "input#division_colour2[name=?]", "division[colour2]"
     end
+  end
+  
+  it "renders the delete button" do
+    # <%= link_to t(".permanently_delete"),division_path(@division), method: :delete, class: "btn btn-danger ", style: "margin: 15px; color: white", data: {confirm: t(".permanently_delete_confirm")} %>
+    render
+    
+    assert_select(".btn.btn-danger[data-method=delete][data-confirm='Are you really really sure?']")
   end
 end
