75390761fbef1f6a64cd0e8d7e8f08237fd24cc4
Luke Rohde <lukerohde@gmail.com>
2017-02-23 14:40:07 +1100

    add example application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-union = Union.new( name: "International Union of Food, Agricultural, Hotel, Restaurant, Catering, Tobacco and Allied Workers' Association", www: "iuf.org", short_name: "IUF")
+union = Union.new(short_name: "IUF", name: "International Union of Food, Agricultural, Hotel, Restaurant, Catering, Tobacco and Allied Workers' Association", www: "iuf.org")
 union.save(validate: false)
 user = Person.create!( email: "admin@iuf.org", password: "temptemp", password_confirmation: "temptemp", first_name: "Admin", union: union )
 user.update!(invited_by: user, authorizer: user) # invite self for the sake of looking like a user
